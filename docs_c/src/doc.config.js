5261935f7a5879bd97de6b8df544c9611417a2f7
chuangye.zhang <chuangye.zhang@zymobi.com>
2018-10-29 17:30:46 +0800

    add
    add

--- docs_c/src/doc.config.js
+++ docs_c/src/doc.config.js
@@ -17,9 +17,9 @@ module.exports = {
         href: '#/'
       },
       nav: {
-          '介绍': 'http://0.0.0.0:8080/#/zh-CN/introIntr',
-          'Vue 组件': 'http://0.0.0.0:8080/#/zh-CN/view',
-          'api 接口': 'http://0.0.0.0:8080/#/zh-CN/requsetApi',
+          '介绍': 'https://appcandoc.github.io/acuidoc_/#/zh-CN/intro',
+          'Vue 组件': 'https://appcandoc.github.io/acuidoc_/#/zh-CN/view',
+          'api 接口': 'https://appcandoc.github.io/acuidoc_/#/zh-CN/requsetApi',
       },
     },
     nav:{
